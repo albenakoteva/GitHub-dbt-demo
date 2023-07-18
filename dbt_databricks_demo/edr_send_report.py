db20ba79a4d907636814c9809edaade89cfe037f
Albena Koteva-Pavlidis <albena.koteva-pavlidis@mentormate.com>
2023-07-18 12:46:22 +0300

    add configuration to 'edr send-report' command

--- dbt_databricks_demo/edr_send_report.py
+++ dbt_databricks_demo/edr_send_report.py
@@ -1,8 +1,9 @@
 import subprocess
 
 
-def run_edr_send_report():
-    command = "edr send-report --slack-token xoxb-5575323113526-5575412966566-SnTA5IdZ8nvl7umWCC9nC62e --slack-channel-name data-accelerator-dbt-demo"
+def run_edr_send_report(slack_token, slack_channel_name):
+
+    command = "edr send-report"
     subprocess.run(command, shell=True)
 
 
